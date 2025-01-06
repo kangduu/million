cfdaffc3c34f5fb97088060b33cbe5e40e3be8ab
dukang <dk@nosugartech.com>
2025-01-06 18:01:10 +0800

    todo: captcha with expression-session and svg-captcha

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,12 @@ import userRoutes from "./userRoutes";
 import lotteryRoutes from "./lotteryRoutes";
 import databaseRoutes from "./databaseRoutes";
 import { verifyToken } from "../middleware/auth";
+import captchaRoutes from "./captchaRoutes";
 
 function insertRoutes(app: Express) {
   const API_VERSION_1 = `/api/${process.env.VERSION_1}`;
+
+  app.use(`${API_VERSION_1}/captcha`, captchaRoutes);
   app.use(`${API_VERSION_1}/user`, userRoutes);
   app.use(`${API_VERSION_1}/lottery`, verifyToken, lotteryRoutes);
   app.use(`${API_VERSION_1}/database`, verifyToken, databaseRoutes);
