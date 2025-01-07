0e3ea63acc61f20eced5aaa25d9ef29a02e21c5a
dukang <dk@nosugartech.com>
2025-01-07 15:24:34 +0800

    feat：登录退出流程实现，captcha实现

--- public/index.js
+++ public/index.js
@@ -1,4 +1,7 @@
 (function () {
+  const store = new Map();
+
+  // 为更新按钮添加点击事件
   function appendUpdateButtonsClick(e) {
     function updateLotteryData(type) {
       const token = localStorage.getItem("token");
@@ -24,20 +27,77 @@
     });
   }
 
-  function login(e) {
+  // 获取验证码
+  function getCaptcha(e) {
+    const captchaImg = e.target;
+    fetch(`http://localhost:9000/api/v1/user/captcha`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then(({ code, data }) => {
+        if (code === 200) {
+          const base64Data = btoa(data.img);
+          const url = `data:image/svg+xml;base64,${base64Data}`;
+          captchaImg.src = url;
+          store.set("captchaId", data.id);
+        }
+      });
+  }
+
+  // 获取表单数据
+  function getFormData(form) {
+    const username = (document.getElementById("username").value || "").trim();
+    const password = (document.getElementById("password").value || "").trim();
+    const captcha = (document.getElementById("captcha").value || "").trim();
+    return { username, password, captcha };
+  }
+
+  // 重置表单
+  function resetForm() {
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("captcha").value = "";
+  }
+
+  // 存储用户数据
+  function storeUserData(data) {
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("username", data.username);
+    localStorage.setItem("expired_at", data.expired_at);
+  }
+
+  // 登录
+  function handleLogin(e) {
     e.preventDefault();
     const loginButton = e.target;
     loginButton.disabled = true;
     loginButton.textContent = "登录中...";
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    console.log(username, password);
+    const { username, password, captcha } = getFormData();
 
-    if (!username || !password) {
+    let verified = false,
+      tip = "";
+
+    if (!username) {
+      verified = true;
+      tip = "请输入用户名";
+    }
+    if (!password && !verified) {
+      verified = true;
+      tip = "请输入密码";
+    }
+    if (!captcha && !verified) {
+      verified = true;
+      tip = "请输入验证码";
+    }
+
+    if (verified) {
+      alert(tip);
       loginButton.disabled = false;
       loginButton.textContent = "登录";
-      alert("请输入用户名和密码");
       return;
     }
 
@@ -46,28 +106,98 @@
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({
+        username,
+        password,
+        captcha,
+        captchaId: store.get("captchaId"),
+      }),
     })
       .then((res) => res.json())
-      .then((data) => {
-        if (data.code === 200) {
-          localStorage.setItem("token", data.data.token);
+      .then(({ code, data, msg }) => {
+        if (code === 200) {
+          resetForm();
+          storeUserData(data);
+          hiddenLogin();
           appendUpdateButtonsClick();
-        }
+          logoutButtonAppendClickEvent();
+        } else alert(msg);
       });
 
     loginButton.disabled = false;
     loginButton.textContent = "登录";
   }
 
+  // 退出登录
+  function logout() {
+    fetch(`http://localhost:9000/api/v1/user/logout`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then(({ code, msg }) => {
+        if (code === 200) {
+          localStorage.clear();
+          showLogin();
+          loginButtonAppendClickEvent();
+          captchaImgAppendClickEvent();
+        } else alert(msg);
+      });
+  }
+
+  // 隐藏登录表单
+  function hiddenLogin() {
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("updates").style.display = "block";
+    document.getElementById("logout").style.display = "block";
+  }
+
+  // 显示登录表单
+  function showLogin() {
+    document.getElementById("login-form").style.display = "block";
+    document.getElementById("updates").style.display = "none";
+    document.getElementById("logout").style.display = "none";
+  }
+
+  function EnterPress(e) {
+    if (e.key === "Enter") handleLogin(e);
+  }
+
+  // 为登录按钮添加点击事件
+  function loginButtonAppendClickEvent() {
+    const loginButton = document.getElementById("login-button");
+    loginButton.addEventListener("click", handleLogin, false);
+
+    window.removeEventListener("keydown", EnterPress, false); // 防止重复绑定 EnterPress 事件
+    window.addEventListener("keydown", EnterPress, false);
+  }
+
+  // 为验证码图片添加点击事件
+  function captchaImgAppendClickEvent() {
+    const captchaImg = document.getElementById("captcha-img");
+    captchaImg.addEventListener("click", getCaptcha, false);
+    captchaImg.click();
+  }
+
+  // 为退出按钮添加点击事件
+  function logoutButtonAppendClickEvent() {
+    const logoutButton = document.getElementById("logout-button");
+    logoutButton.addEventListener("click", logout, false);
+  }
+
+  // 初始化
   function init() {
     const token = localStorage.getItem("token");
     if (token) {
+      hiddenLogin();
       appendUpdateButtonsClick();
-      document.getElementById("login-form").style.display = "none";
+      logoutButtonAppendClickEvent();
     } else {
-      const loginButton = document.getElementById("login-button");
-      loginButton.addEventListener("click", login);
+      showLogin();
+      loginButtonAppendClickEvent();
+      captchaImgAppendClickEvent();
     }
   }
 
